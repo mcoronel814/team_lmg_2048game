1f0a634b19c975c20b6d35625c6976038ac49dc3
Manuel Coronel <mcoronel814@hotmail.com>
2022-07-10 17:09:02 -0400

    created window size for the game

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-#lets start
+import pygame
+
+class game2048:
+    def __init__(self):
+        self.window_width = 600
+        self.window_height = 750
+
+        pygame.init()
+
+        self.window = pygame.display.set_mode((self.window_width, self.window_height))
+
+    def play(self):
+        playing = True
+        while playing:
+            pygame.display.update()
+
+
+if __name__ == "__main__":
+    game = game2048()
+    game.play()
