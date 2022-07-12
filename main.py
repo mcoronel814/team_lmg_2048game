af7ceaf3fa0c2773b30ef94cd9b16a37829831b1
Manuel Coronel <mcoronel814@hotmail.com>
2022-07-11 20:54:59 -0400

    updated window to display actual board with blocks

--- main.py
+++ main.py
@@ -1,20 +1,47 @@
 import pygame
 
-class game2048:
+
+class game_2048:
     def __init__(self):
-        self.window_width = 600
-        self.window_height = 750
+        self.board_length = 4
+        self.cell_size = 100
+        self.gap = 5
+        self.window_bg_color = (187, 173, 160)
+        self.block_size = self.cell_size + self.gap * 2
+
+        self.window_width = self.block_size * 6
+        self.window_height = self.block_size * 4
 
         pygame.init()
 
         self.window = pygame.display.set_mode((self.window_width, self.window_height))
 
+    def draw_board(self):
+        self.window.fill(self.window_bg_color)
+
+        for r in range(self.board_length):
+            rect_y = self.block_size * r + self.gap
+            for c in range(self.board_length):
+                rect_x = self.block_size * c + self.gap
+
+                pygame.draw.rect(
+                    self.window,
+                    (0, 0, 0),
+                    pygame.Rect(rect_x, rect_y, self.cell_size, self.cell_size)
+                )
+
+
     def play(self):
         playing = True
         while playing:
+            self.draw_board()
             pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    playing = False
 
 
 if __name__ == "__main__":
-    game = game2048()
+    game = game_2048()
     game.play()
