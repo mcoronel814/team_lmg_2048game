7898a5f811a61f1ffa0252c98960ab98af03d1b9
Manuel Coronel <mcoronel814@hotmail.com>
2022-08-02 19:36:24 -0400

    put window caption within the game_2048 class. added the display of numbers within the drawboard function. added game over function. -Tianna & Manuel

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import pygame
 import random
 import numpy as np
 
-pygame.display.set_caption('2048 - brought to you by LMG')
+# Background color's dictionary goes here
+
 
 class game_2048:
     def __init__(self):
@@ -18,6 +19,8 @@ class game_2048:
         pygame.init()
 
         self.window = pygame.display.set_mode((self.window_width, self.window_height))
+        self.myFont = pygame.font.SysFont("Comic Sans MS", 30)
+        pygame.display.set_caption('2048 - brought to you by LMG')
 
         # start the board with zeros and random number
         self.board_status = np.zeros((self.board_length, self.board_length))
@@ -37,12 +40,18 @@ class game_2048:
             rect_y = self.block_size * r + self.gap
             for c in range(self.board_length):
                 rect_x = self.block_size * c + self.gap
+                cell_value = int(self.board_status[r][c])
 
                 pygame.draw.rect(
                     self.window,
-                    (0, 0, 0),
+                    BG_COLORS[cell_value],
                     pygame.Rect(rect_x, rect_y, self.cell_size, self.cell_size)
                 )
+
+                if cell_value != 0:
+                    text_surface = self.myFont.render(f"{cell_value}", True, (0, 0, 0))
+                    text_rect = text_surface.get_rect(center=(rect_x + self.block_size / 2, rect_y + self.block_size / 2))
+                    self.window.blit(text_surface, text_rect)
 
     def merge_numbers(self, data):
         result = [0]
@@ -60,9 +69,9 @@ class game_2048:
         for idx in range(self.board_length):
 
             if dir in "UD":
-                data = self.draw_board[:, idx]
+                data = self.board_status[:, idx]
             else:
-                data = self.draw_board[idx, :]
+                data = self.board_status[idx, :]
 
             flip = False
             if dir in "RD":
@@ -76,32 +85,53 @@ class game_2048:
                 data = data[::-1]
 
             if dir in "UD":
-                self.draw_board[:, idx] = data
+                self.board_status[:, idx] = data
             else:
-                self.draw_board[idx, :] = data
+                self.board_status[idx, :] = data
+
+    def game_over(self):
+        board_status_backup = self.board_status.copy()
+        for dir in "UDLR":
+            self.movement(dir)
+
+            if (self.board_status == board_status_backup).all() == False:
+                self.board_status = board_status_backup
+                return False
+        return True
 
     def play(self):
-        game_active = True
-        playing = True
-        while playing:
+        running = True
+        while running:
             self.draw_board()
             pygame.display.update()
-        while True:
+
             for event in pygame.event.get():
+                old_board_status = self.board_status.copy()
+
                 if event.type == pygame.QUIT:
-                    playing = False
-                    pygame.quit()
-                    exit()
-                if game_active:
-                    if event.type == pygame.KEYDOWN:
-                        if event.key == pygame.K_UP:
-                            print("UP")
-                        elif event.key == pygame.K_DOWN:
-                            print("DOWN")
-                        elif event.key == pygame.K_RIGHT:
-                            print("RIGHT")
-                        elif event.key == pygame.K_LEFT:
-                            print("LEFT")
+                    running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        # print("U")
+                        self.movement("U")
+                    elif event.key == pygame.K_DOWN:
+                        # print("D")
+                        self.movement("D")
+                    elif event.key == pygame.K_LEFT:
+                        # print("L")
+                        self.movement("L")
+                    elif event.key == pygame.K_RIGHT:
+                        # print("R")
+                        self.movement("R")
+                    elif event.key == pygame.K_ESCAPE:
+                        running = False
+
+                    if self.game_over():
+                        print("Game Over !!")
+                        return
+
+                    if (self.board_status == old_board_status).all() == False:
+                        self.add_new_number()
 
 
 if __name__ == "__main__":
