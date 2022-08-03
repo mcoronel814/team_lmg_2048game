20bcda90282b47a7c1b9b0fe1d44a0392d286d37
Leslie Vivaldo <lvivaldo@mercy.edu>
2022-08-03 13:56:51 -0400

    Scores

--- main.py
+++ main.py
@@ -1,176 +1,95 @@
 import pygame
 from sys import exit
 
-pygame.init()
-screen = pygame.display.set_mode((700, 400))
-pygame.display.set_caption('LMGs 2048')
-clock = pygame.time.Clock()
-test_font = pygame.font.Font(None, 50)
 
+class game_2048:
 
-surface = pygame.Surface((700,400))
-surface.fill('beige')
-#pygame.draw.rect(screen, 'Pink',(600,200))
-#block_a1 = pygame.draw.rect(screen, 'Pink',(600,200))
-#block_a2
-#block_a3
-#block_a4
-#block_b1
-#block_b2
-#block_b3
-#block_b4
-#block_c1
-#block_c2
-#block_c3
-#block_c4
-#block_d1
-#block_d2
-#block_d3
-#block_d4
-score_surface = test_font.render("Score:", False, "brown")
+    def __init__(self):
 
-up_button = pygame.image.load("up_key.png").convert_alpha()
-up_arrow = pygame.transform.scale(up_button, (50,50))
-up_rect = up_arrow.get_rect(bottomright = (550, 250))
+        self.board_length = 4
+        self.cell_size = 100
+        self.gap = 5
+        self.window_bg_color = (187, 173, 160)
+        self.block_size = self.cell_size + self.gap * 2
 
-down_button = pygame.image.load("down_key.png").convert_alpha()
-down_arrow = pygame.transform.scale(down_button, (50,50))
-down_rect = down_arrow.get_rect(bottomright =(550, 300))
+        self.window_width = self.block_size * 6
+        self.window_height = self.block_size * 4
 
+        pygame.init()
 
-right_button = pygame.image.load("right_key.png").convert_alpha()
-right_arrow = pygame.transform.scale(right_button, (50,50))
-right_rect = right_arrow.get_rect(bottomright =(600, 300))
+        self.window = pygame.display.set_mode((self.window_width, self.window_height))
 
-left_button = pygame.image.load("left_key.png").convert_alpha()
-left_arrow = pygame.transform.scale(left_button, (50,50))
-left_rect = left_arrow.get_rect(bottomright =(500, 300))
+    def draw_board(self):
+        self.window.fill(self.window_bg_color)
 
+        for r in range(self.board_length):
+            rect_y = self.block_size * r + self.gap
+            for c in range(self.board_length):
+                rect_x = self.block_size * c + self.gap
 
+                pygame.draw.rect(
+                    self.window,
+                    (0, 0, 0),
+                    pygame.Rect(rect_x, rect_y, self.cell_size, self.cell_size)
+                )
 
+    def side_panel(self):
+        for box in range(self.board_length):
+            surface = pygame.Surface((0, 0))
+            self.window.blit(surface, (0, 0))
 
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
-            exit()
+    def play(self):
+        playing = True
+        while playing:
+            up_button = pygame.image.load("up_key.png").convert_alpha()
+            up_arrow = pygame.transform.scale(up_button, (50, 50))
+            up_rect = up_arrow.get_rect(bottomright=(575, 325))
 
-     #KEYBOARD CONTROLS
-    if event.type == pygame.KEYDOWN:
-        if event.key == pygame.K_UP:
-            print("UP")
-    # block_gravity = -15
-        elif event.key == pygame.K_DOWN:
-           print("DOWN")
-        elif event.key == pygame.K_RIGHT:
-           print("RIGHT")
-        elif event.key == pygame.K_LEFT:
-            print("LEFT")
+            down_button = pygame.image.load("down_key.png").convert_alpha()
+            down_arrow = pygame.transform.scale(down_button, (50, 50))
+            down_rect = down_arrow.get_rect(bottomright=(575, 380))
 
-    #MOUSE CONTROLS
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        mouse_pos = pygame.mouse.get_pos()
-        print(event.pos)
-        if up_rect.collidepoint(mouse_pos):
-            print('up')
-        if down_rect.collidepoint(mouse_pos):
-            print("down")
-        if right_rect.collidepoint(mouse_pos):
-            print("right")
-        if left_rect.collidepoint(mouse_pos):
-            print("left")
+            right_button = pygame.image.load("right_key.png").convert_alpha()
+            right_arrow = pygame.transform.scale(right_button, (50, 50))
+            right_rect = right_arrow.get_rect(bottomright=(630, 380))
 
-    #draw all our elements
-    #update everything
-    screen.blit(surface,(0,0))
-    screen.blit(up_arrow, up_rect)
-    screen.blit(down_arrow, down_rect)
-    screen.blit(right_arrow, right_rect)
-    screen.blit(left_arrow, left_rect)
-    screen.blit(score_surface,(500, 50))
+            left_button = pygame.image.load("left_key.png").convert_alpha()
+            left_arrow = pygame.transform.scale(left_button, (50, 50))
+            left_rect = left_arrow.get_rect(bottomright=(520, 380))
 
-    pygame.display.update()
-    clock.tick(0)
+            self.draw_board()
+            self.window.blit(up_arrow, up_rect)
+            self.window.blit(down_arrow, down_rect)
+            self.window.blit(right_arrow, right_rect)
+            self.window.blit(left_arrow, left_rect)
 
-
-
-#class game_2048:
-    #def __init__(self):
-    #self.board_length = 4
-    #self.cell_size = 100
-    #self.gap = 5
-    #self.window_bg_color = (187, 173, 160)
-    #self.block_size = self.cell_size + self.gap * 2
-
-    #self.window_width = self.block_size * 6
-    #self.window_height = self.block_size * 4
-
-       # pygame.init()
-
-
-        # block_gravity = 0
-
-
-        #self.window = pygame.display.set_mode((self.window_width, self.window_height))
-
-    #def draw_board(self):
-       # self.window.fill(self.window_bg_color)
-
-        #for r in range(self.board_length):
-            #rect_y = self.block_size * r + self.gap
-            #for c in range(self.board_length):
-               # rect_x = self.block_size * c + self.gap
-
-               # pygame.draw.rect(
-                    #self.window,
-                    #(0, 0, 0),
-                   # pygame.Rect(rect_x, rect_y, self.cell_size, self.cell_size)
-              #  )
-
-    #def play(self):
-        #playing = True
-        #while playing:
-            #self.draw_board()
-           # pygame.display.update()
-
-            #for event in pygame.event.get():
-                #if event.type == pygame.QUIT:
-                    #playing = False
-
-            #up_button()
-            #pygame.display.update()
-
+            pygame.display.update()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    exit()
                 # KEYBOARD CONTROLS
-            #if event.type == pygame.KEYDOWN:
-                    #if event.key == pygame.K_UP:
-                      #  print("UP")
-                        # block_gravity = -15
-                    #elif event.key == pygame.K_DOWN:
-                       # print("DOWN")
-                    #elif event.key == pygame.K_RIGHT:
-                     #   print("RIGHT")
-                    #elif event.key == pygame.K_LEFT:
-                      #  print("LEFT")
-
-           # if event.type == pygame.MOUSEMOTION:
-                   # print(event.pos)
-
-                    # if event.type == MOUSEBUTTONDOWN:
-                    # mouse_pos = pygame.mouse.get_pos:
-
-                    # if up_rect.collidepoint(mouse_pos):
-                    # direction = "up"
-                # if down_rect.collidepoint(mouse_pos):
-                # direction = "down"
-                # if right_rect.collidepoint(mouse_pos):
-                # direction = "right"
-                # if left_rect.collidepoint(mouse_pos):
-                # direction = "left"
-
-            # if direction == "down":
-            # block_gravity +=
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_UP:
+                        print("UP")
+                    # block_gravity = -15
+                    elif event.key == pygame.K_DOWN:
+                        print("DOWN")
+                    elif event.key == pygame.K_RIGHT:
+                        print("RIGHT")
+                    elif event.key == pygame.K_LEFT:
+                        print("LEFT")
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if up_rect.collidepoint(event.pos):
+                        print("UP")
+                    if down_rect.collidepoint(event.pos):
+                        print("DOWN")
+                    if right_rect.collidepoint(event.pos):
+                        print("RIGHT")
+                    if left_rect.collidepoint(event.pos):
+                        print("LEFT")
 
 
-#if __name__ == "__main__":
-    #game = game_2048()
-    #game.play()
+if __name__ == "__main__":
+    game = game_2048()
+    game.play()
